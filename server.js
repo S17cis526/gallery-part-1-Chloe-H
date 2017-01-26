ebe3babcb9ecaff628d8ccbf208b5f045274cd0b
Chloe <chenderson126+bitbucket@gmail.com>
2017-01-26 14:16:10 -0600

    Completed a tiny, crappy web server in node.

--- server.js
+++ server.js
@@ -1,6 +1,65 @@
+"use strict";
+
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+
+// import the given module (?)
+var http = require('http');
+var fs = require('fs');
+
+var port = 3000;
+
+function serveImage(filename, req, res) {
+    fs.readFile('images/' + filename, function(err, body) {
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.statusMessage = "ooooooops"
+            res.end("OH NO");
+            return;
+        }
+
+        res.setHeader("Content-Type", "image/jpeg");
+        res.end(body);
+    });
+}
+
+// create a server
+var server = http.createServer(function(req, res) {
+    // anon fcn uses createServer's context instead of creating its own scope
+    // you can totally mess with the method's code this way using 'this'
+
+    /* serveImage(req.url, req, res);
+     * don't do this, it's insecure
+     */
+
+    switch(req.url) {
+        case "/chess":
+        case "/chess/":
+        case "/chess.jpg":
+        case "/chess.jpeg":
+            serveImage('chess.jpg', req, res);
+            break;
+        case "/fern":
+        case "/fern/":
+        case "/fern.jpg":
+        case "/fern.jpeg":
+            serveImage('fern.jpg', req, res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.statusMessage = "Not found";
+            res.end();
+    }
+});
+
+// listen to the given port
+server.listen(port, function() {
+    console.log("listening on Port " + port);
+});
+
+// semicolons AND newline characters delimit lines - don't need both
+// you can put multiple lines on one line if you use semicolons, though
